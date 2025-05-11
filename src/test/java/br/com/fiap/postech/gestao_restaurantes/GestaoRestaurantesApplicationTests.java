2b0a94016b45d94c17a930757c55d240ae5b443b
Michael Barroso <michael_p._b.15600@hotmail.com>
2025-05-10 22:45:04 -0300

    #1 feat: Criação do projeto.
    - Criação do projeto utilizando initialz do Spring.
    - Adicionado configuração de conexão com banco postgres no properties.

---
+++ src/test/java/br/com/fiap/postech/gestao_restaurantes/GestaoRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.gestao_restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
